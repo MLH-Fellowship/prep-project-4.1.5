8193306feca4e339a1afeef20e46b65b4737e2df
Damans227 <61474540+Damans227@users.noreply.github.com>
2021-11-18 19:47:55 -0500

    set user location automatically on initial render

--- src/App.js
+++ src/App.js
@@ -2,18 +2,37 @@ import { useEffect, useState } from "react";
 import "./App.css";
 import WeatherMap from "./components/map/weather-map";
 import logo from "./mlh-prep.png";
+import axios from "axios";
 
 function App() {
   const [error, setError] = useState(null);
   const [isLoaded, setIsLoaded] = useState(false);
-  const [city, setCity] = useState("New York City");
+  const [city, setCity] = useState("");
   const [results, setResults] = useState(null);
   const [lat, setLat] = useState("");
   const [long, setLong] = useState("");
 
-  const BASE_URL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${process.env.REACT_APP_APIKEY}`;
+  //reverse geolocation to initialize current city
+  function initialize(lat, long) {
+    const url = `http://api.openweathermap.org/geo/1.0/reverse?lat=${lat}&lon=${long}&limit=1&appid=${process.env.REACT_APP_APIKEY}`;
+
+    axios.get(url).then((res) => {
+      console.log(res);
+      setCity(res.data[0].name);
+    });
+  }
 
   useEffect(() => {
+    navigator.geolocation.getCurrentPosition((pos) => {
+      let x = pos.coords.latitude;
+      let y = pos.coords.longitude;
+      initialize(x, y);
+    });
+  }, []);
+
+  useEffect(() => {
+    const BASE_URL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${process.env.REACT_APP_APIKEY}`;
+
     fetch(BASE_URL)
       .then((res) => res.json())
       .then(
@@ -35,7 +54,7 @@ function App() {
           setError(error);
         }
       );
-  }, [city, BASE_URL]);
+  }, [city]);
 
   if (error) {
     return <div>Error: {error.message}</div>;
