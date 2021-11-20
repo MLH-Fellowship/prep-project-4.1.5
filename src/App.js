48daf333fb3632c09c73ae54d6bfdf4d66fbf3c7
Daman Arora <61474540+Damans227@users.noreply.github.com>
2021-11-20 02:07:29 -0500

    Set user location automatically on initial render (#12)
    * Adding a map to display location
    
    * adding leaflet
    
    * Styling the map
    
    * Adding dynamic location coordinates
    
    * Added city prop to the map
    
    * Changed styling on the map
    
    * Refactored API URL in root element
    
    * Add BASE_URL as react dependency for useEffect hook
    
    * set user location automatically on initial render
    
    * changing http to https for api url
    
    * allow valid city name
    
    * fixed merge conflicts
    
    * restore example env in root dir

--- src/App.js
+++ src/App.js
@@ -1,56 +1,98 @@
 import { useEffect, useState } from "react";
-import './App.css';
-import logo from './mlh-prep.png'
+import "./App.css";
+import WeatherMap from "./components/map/weather-map";
+import logo from "./mlh-prep.png";
+import axios from "axios";
 
 function App() {
   const [error, setError] = useState(null);
   const [isLoaded, setIsLoaded] = useState(false);
-  const [city, setCity] = useState("New York City")
+  const [city, setCity] = useState("");
   const [results, setResults] = useState(null);
+  const [lat, setLat] = useState("");
+  const [long, setLong] = useState("");
+
+  //reverse geolocation to initialize current city
+  function initialize(lat, long) {
+    const url = `https://api.openweathermap.org/geo/1.0/reverse?lat=${lat}&lon=${long}&limit=1&appid=${process.env.REACT_APP_APIKEY}`;
+
+    axios.get(url).then((res) => {
+      setCity(res.data[0].name);
+    });
+  }
 
   useEffect(() => {
-    fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric" + "&appid=" + process.env.REACT_APP_APIKEY)
-      .then(res => res.json())
-      .then(
-        (result) => {
-          if (result['cod'] !== 200) {
-            setIsLoaded(false)
-          } else {
+    navigator.geolocation.getCurrentPosition((pos) => {
+      let x = pos.coords.latitude;
+      let y = pos.coords.longitude;
+      initialize(x, y);
+    });
+  }, []);
+
+  useEffect(() => {
+    const BASE_URL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${process.env.REACT_APP_APIKEY}`;
+
+    if (city) {
+      fetch(BASE_URL)
+        .then((res) => res.json())
+        .then(
+          (result) => {
+            if (result["cod"] !== 200) {
+              setIsLoaded(false);
+            } else {
+              setIsLoaded(true);
+              setResults(result);
+
+              const { coord } = result; //long lat
+              const { lat, lon } = coord;
+              setLat(lat);
+              setLong(lon);
+            }
+          },
+          (error) => {
             setIsLoaded(true);
-            setResults(result);
+            setError(error);
           }
-        },
-        (error) => {
-          setIsLoaded(true);
-          setError(error);
-        }
-      )
-  }, [city])
+        );
+    } else {
+      return;
+    }
+  }, [city]);
 
   if (error) {
     return <div>Error: {error.message}</div>;
   } else {
-    return <>
-      <img className="logo" src={logo} alt="MLH Prep Logo"></img>
-      <div>
-        <h2>Enter a city below 👇</h2>
-        <input
-          type="text"
-          value={city}
-          onChange={event => setCity(event.target.value)} />
-        <div className="ResultsContainer">
-          <div className="Results">
-            {!isLoaded && <h2>Loading...</h2>}
-            {console.log(results)}
-            {isLoaded && results && <>
-              <h3>{results.weather[0].main}</h3>
-              <p>Feels like {results.main.feels_like}°C</p>
-              <i><p>{results.name}, {results.sys.country}</p></i>
-            </>}
+    return (
+      <>
+        <img className="logo" src={logo} alt="MLH Prep Logo"></img>
+        <div>
+          <h2>Enter a city below 👇</h2>
+          <input
+            type="text"
+            value={city}
+            onChange={(event) => setCity(event.target.value)}
+          />
+          <div className="ResultsContainer">
+            <div className="Results">
+              {!isLoaded && <h2>Loading...</h2>}
+              {console.log(results)}
+              {isLoaded && results && (
+                <>
+                  <h3>{results.weather[0].main}</h3>
+                  <p>Feels like {results.main.feels_like}°C</p>
+                  <i>
+                    <p>
+                      {results.name}, {results.sys.country}
+                    </p>
+                  </i>
+                </>
+              )}
+            </div>
           </div>
+          <WeatherMap Lat={lat} Long={long} City={city} />
         </div>
-      </div>
-    </>
+      </>
+    );
   }
 }
 
---
+++ src/components/map/weather-map.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Map, TileLayer, Marker, Popup } from "react-leaflet";
+import "./weather-map.css";
+
+const WeatherMap = (props) => {
+  return (
+    <div className="map-container">
+      <Map className="map" center={[props.Lat, props.Long]} zoom={2}>
+        <TileLayer
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={[props.Lat, props.Long]}>
+          <Popup>{props.City}</Popup>
+        </Marker>
+      </Map>
+    </div>
+  );
+};
+
+export default WeatherMap;
