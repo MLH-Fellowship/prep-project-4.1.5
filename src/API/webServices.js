7e5d73f6dd870dd84a34a974b98b82de9d554c00
abdumamdouh <eabdogfx@gmail.com>
2021-11-23 01:53:49 +0200

    implement getCityResults using long & lat & unit

--- src/API/webServices.js
+++ src/API/webServices.js
@@ -18,3 +18,24 @@ export const getCityCoordinates = async (city) => {
         })
     });
 }
+
+// pass 'F' for "Fahrenheit" and 'C' for "Celsius"
+export const getCityResults = async (longitude, latitude, unit) => {
+    unit = unit === 'F' ? "imperial" : "metric";
+    return new Promise( async function(resolve, reject){
+        fetch(
+            `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=${unit}&appid=${process.env.REACT_APP_APIKEY}`
+        )
+            .then(res => res.json())
+            .then(results => {
+            if(results.lentgh === 0) {
+                reject(new Error("Can't find City"))
+            }
+            else {
+                resolve({
+                    results
+                })
+            }
+        })
+    });
+}
