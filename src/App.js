b1bf303b0cb7f4d045a0d3f21f3a16054f154665
Shikhar15606 <shikharshukla15606@gmail.com>
2021-11-25 15:14:22 +0530

    used debounce time to reduce unnecessary API calls

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useMemo, useCallback } from "react";
+import debounce from 'lodash.debounce';
 import "./App.css";
 
 import WeatherMap from "./components/map/weather-map";
@@ -10,62 +11,72 @@ import HourlyWeather from "./Components/HourlyWeather/index";
 import "./Components/HourlyWeather/index.css";
 
 function App() {
-	const [error, setError] = useState(null);
-	const [isLoaded, setIsLoaded] = useState(false);
-	const [city, setCity] = useState("");
-	const [results, setResults] = useState(null);
-	const [lat, setLat] = useState("");
-	const [long, setLong] = useState("");
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [city, setCity] = useState("");
+  const [results, setResults] = useState(null);
+  const [lat, setLat] = useState("");
+  const [long, setLong] = useState("");
 
-	//reverse geolocation to initialize current city
-	function initialize(lat, long) {
-		const url = `https://api.openweathermap.org/geo/1.0/reverse?lat=${lat}&lon=${long}&limit=1&appid=${process.env.REACT_APP_APIKEY}`;
+  //reverse geolocation to initialize current city
+  function initialize(lat, long) {
+    const url = `https://api.openweathermap.org/geo/1.0/reverse?lat=${lat}&lon=${long}&limit=1&appid=${process.env.REACT_APP_APIKEY}`;
 
-		axios.get(url).then((res) => {
-			setCity(res.data[0].name);
-		});
-	}
+    axios.get(url).then((res) => {
+      setCity(res.data[0].name);
+    });
+  }
 
-	useEffect(() => {
-		navigator.geolocation.getCurrentPosition((pos) => {
-			let x = pos.coords.latitude;
-			let y = pos.coords.longitude;
-			initialize(x, y);
-		});
-	}, []);
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((pos) => {
+      let x = pos.coords.latitude;
+      let y = pos.coords.longitude;
+      initialize(x, y);
+    });
+  }, []);
 
-	useEffect(() => {
-		const BASE_URL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${process.env.REACT_APP_APIKEY}`;
+  const fetchData = useCallback(async () => {
+    // console.log(`Fetching for ${city}  .....`);
+    const BASE_URL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${process.env.REACT_APP_APIKEY}`;
+    fetch(BASE_URL)
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          if (result["cod"] !== 200) {
+            setIsLoaded(false);
+          } else {
+            setIsLoaded(true);
+            setResults(result);
 
-		if (city) {
-			const fetchData = async () => {
-				fetch(BASE_URL)
-					.then((res) => res.json())
-					.then(
-						(result) => {
-							if (result["cod"] !== 200) {
-								setIsLoaded(false);
-							} else {
-								setIsLoaded(true);
-								setResults(result);
+            const { coord } = result; //long lat
+            const { lat, lon } = coord;
+            setLat(lat);
+            setLong(lon);
+          }
+        },
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      );
+  }, [city, setIsLoaded, setResults, setLat, setError, setLong])
 
-								const { coord } = result; //long lat
-								const { lat, lon } = coord;
-								setLat(lat);
-								setLong(lon);
-							}
-						},
-						(error) => {
-							setIsLoaded(true);
-							setError(error);
-						}
-					);
-			};
-			fetchData();
-		} else {
-			return;
-		}
-	}, [city]);
+  const debouncedEventHandler = useMemo(() => debounce(fetchData, 1000), [fetchData]);
+
+  useEffect(() => {
+    if (city) {
+      debouncedEventHandler();
+    } else {
+      return;
+    }
+  }, [city, debouncedEventHandler]);
+
+  useEffect(() => {
+    // cleanup
+    return () => {
+      debouncedEventHandler.cancel();
+    }
+  }, [debouncedEventHandler]);
 
 
   if (error) {
@@ -79,7 +90,7 @@ function App() {
           <input
             type="text"
             value={city}
-            onChange={(event) => setCity(event.target.value)}
+            onChange={event => { setCity(event.target.value) }}
           />
           <div className="ResultsContainer">
             <div className="Results">
@@ -100,15 +111,15 @@ function App() {
           </div>
           <WeatherMap Lat={lat} Long={long} City={city} />
           <div className="hourly-weather">
-          {!isLoaded && <h2>Loading...</h2>}
+            {!isLoaded && <h2>Loading...</h2>}
             {console.log(results)}
             {isLoaded && results && <>
-              <HourlyWeather placename={ results.name } />
+              <HourlyWeather placename={results.name} />
             </>}
           </div>
-             <div className='AccesorySuggestion'>
-						{results ? <AccesorySuggestion results={results} /> : <h2></h2>}
-					</div>
+          <div className='AccesorySuggestion'>
+            {results ? <AccesorySuggestion results={results} /> : <h2></h2>}
+          </div>
         </div>
       </>
     );
